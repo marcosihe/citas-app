b7932333ce60e0fd8c1c569fbb523789030e6c98
unknown <hemrcs@gmail.com>
2022-06-24 20:21:38 -0300

    #001: Project set up
    Add tailwindcss to the project.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className='text-4xl font-bold uppercase text-indigo-500'>Header</h1>
+    </header>
+  )
+}
+
+export default Header

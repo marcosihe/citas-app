036dc8acd97de22293139df505febb34e62f558f
unknown <hemrcs@gmail.com>
2022-06-25 16:39:15 -0300

    #005 main-listado-pacientes
    Create Paciente component
    Add styles to ListadoPaciente and Paciente components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   return (
     <div>
       <Header />
-      <div>
+      <div className="mt-12 md:flex">
         <Formulario />
+        <ListadoPacientes />
       </div>
     </div>
   );
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,7 +1,32 @@
 const Paciente = () => {
   return (
-    <div>Paciente</div>
-  )
-}
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Tau</span>
+      </p>
 
-export default Paciente
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Juan Perez</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">example@email.com</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
+        <span className="font-normal normal-case">25/06/2022</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: {""}
+        <span className="font-normal normal-case">Tos seca</span>
+      </p>
+    </div>
+  );
+};
+
+export default Paciente;
